87ed6a32883a85e9216c0890f0b3903ddec0b907
ghberemeskels <ghberemeskels@xavier.edu>
2017-05-25 22:59:04 -0400

    Dwarf Underground

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import MainContent from './MainContent'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <MainContent />
+
+        <footer className="expanded row">
+          <h6>&copy; 2017 Erebor Industries</h6>
+          <h6 className="float-right">All rights reserved</h6>
+        </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
